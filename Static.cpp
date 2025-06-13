21aacaccc54fd1155be50c7c2118434fc750db8a
Trishna Paswan <S24CSEU0083@bennett.edu.in>
2025-06-14 03:32:37 +0530

    Create Static.cpp

---
+++ Static.cpp
@@ -0,0 +1,45 @@
+// STATIC KEYWORD -> Function 
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+void fun(){
+    static int x = 0; // init statement -> 1 run
+    cout << "x : " << x << endl;
+    x++;
+}
+int main(){
+    fun();
+    fun();
+    return 0;
+}
+
+// STATIC KEYWORD -> Class
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+void fun(){
+    static int x = 0; // init statement -> 1 run
+    cout << "x : " << x << endl;
+    x++;
+}
+
+class A{
+    public:
+    int x;
+    void incX(){
+        x = x + 1;
+    }
+};
+
+int main(){
+    A obj;
+    obj.x = 0;
+    cout << obj.x << endl;
+    obj.incX();
+    cout << obj.x << endl;
+    return 0;
+}
