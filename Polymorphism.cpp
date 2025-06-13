e429b9df812c2f143ae6c4a4a66309e9baf9c866
Trishna Paswan <S24CSEU0083@bennett.edu.in>
2025-06-14 03:07:46 +0530

    Update Polymorphism.cpp

--- Polymorphism.cpp
+++ Polymorphism.cpp
@@ -26,3 +26,25 @@ int main(){
     return 0;
 }
 
+// FUNCTION OVERLOADING  -> COMPILE TIME 
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Print {
+public:
+    void Show(int x){
+        cout << "int " << x << endl;
+
+    }
+    void show(char ch){
+        cout << "char " << ch << endl;
+    }
+};
+
+int main(){
+    Print p1;
+    p1.Show(101);
+    return 0;
+}
+
