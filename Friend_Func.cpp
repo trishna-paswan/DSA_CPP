df48f235a38bbeb7954fb725619731d76019b772
Trishna Paswan <S24CSEU0083@bennett.edu.in>
2025-06-14 03:38:23 +0530

    Create Friend_Func.cpp

---
+++ Friend_Func.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class Box {
+private:
+    int length;
+
+public:
+    Box() : length(0) {}
+
+    // Declare friend function
+    friend void setLength(Box &b, int l);
+};
+
+// Friend function definition
+void setLength(Box &b, int l) {
+    b.length = l;  // Accessing private member
+    cout << "Length is set to: " << b.length << endl;
+}
+
+int main() {
+    Box b;
+    setLength(b, 10);  // Can access private member
+    return 0;
+}
