b212cd977040b5de86048b36302129de4daf03e4
Trishna Paswan <S24CSEU0083@bennett.edu.in>
2025-06-14 03:24:24 +0530

    Create Abstract.cpp

---
+++ Abstract.cpp
@@ -0,0 +1,22 @@
+// ABSTRACT CLASSES
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class Shape {   // Abstract class -> couldn't make a obj of this class
+    virtual void draw() = 0; // Pure virtual function
+};
+
+class Circle : public Shape {
+    public:
+    void draw(){
+        cout << "Drawing a circle \n" << endl;
+    }
+};
+
+int main(){
+    Circle p1;
+    p1.draw();
+    return 0;
+}
