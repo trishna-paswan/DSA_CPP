bf7fce37330407109b7d572e1b39e4bef5e698bd
Trishna Paswan <S24CSEU0083@bennett.edu.in>
2025-07-24 23:40:41 +0530

    Update vectors.cpp

--- vectors.cpp
+++ vectors.cpp
@@ -6,7 +6,7 @@ int main() {
     vector<int> vec = {1,2,3};    
     cout << vec[0] << endl;
      
-    vector<int> vec (3,0);  //output, 0 value for 3 size vector
+    vector<int> vec (3,0);  //output, 0 value for 3 size vector 
     cout << vec[0] << endl;
     cout << vec[1] << endl; 
     cout << vec[2] << endl;
