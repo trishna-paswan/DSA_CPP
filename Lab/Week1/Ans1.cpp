efde84e5ea7f6af86e363dd084400eb84888f2a5
Trishna Paswan <S24CSEU0083@bennett.edu.in>
2025-08-01 10:02:53 +0530

    Update Ans1.cpp

--- Lab/Week1/Ans1.cpp
+++ Lab/Week1/Ans1.cpp
@@ -1,4 +1,4 @@
-/*  
+/*   
 
 Question 1: 
 Problem Statement: Imagine you're building a simple student information system for your class. Each student should be able to introduce themselves and enter their marks in two subjects. The system should then display their total score. 
